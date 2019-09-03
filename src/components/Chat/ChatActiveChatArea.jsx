7964ba85057b20605f80f9c74860b0153159e4b3
Mihail <mihail.pogosyan96@gmail.com>
2019-09-03 23:44:48 +0300

    fix permissions

--- src/components/Chat/ChatActiveChatArea.jsx
+++ src/components/Chat/ChatActiveChatArea.jsx
@@ -3,9 +3,10 @@ import { connect } from 'react-redux';
 import { Grid, Card, CardContent, CardActions, TextField, Button } from '@material-ui/core';
 import { withStyles } from '@material-ui/styles';
 import classNames from 'classnames';
+import _get from 'lodash.get';
 import { sendMessage, setMessageText, chatMessageText } from '../../store/chat';
 import { compose } from 'recompose';
-import { userProfileSelector } from '../../store/auth';
+import { userProfileSelector, userPermissionsSelector } from '../../store/auth';
 const styles = theme => ({
   messageContainer: {
     marginBottom: theme.spacing(2)
@@ -47,7 +48,8 @@ class ActiveChannelArea extends PureComponent {
     dispatch(sendMessage());
   };
   render() {
-    const { classes, messages, messageText, userId } = this.props;
+    const { classes, messages, messageText, userId, permission } = this.props;
+    const isCreateAllowed = !!_get(permission, 'chat.C', false);
     return (
       <>
         <CardContent className={classes.chatArea}>
@@ -73,32 +75,35 @@ class ActiveChannelArea extends PureComponent {
             ))}
           </Grid>
         </CardContent>
-        <CardActions>
-          <form onSubmit={this.handleSubmit} className={classes.form}>
-            <TextField
-              label="Label"
-              style={{ margin: 8 }}
-              fullWidth
-              margin="normal"
-              variant="filled"
-              InputLabelProps={{
-                shrink: true
-              }}
-              value={messageText}
-              onChange={this.handleChange}
-            />
-            <Button variant="contained" color="primary" type="submit">
-              Отправить
-            </Button>
-          </form>
-        </CardActions>
+        {isCreateAllowed && (
+          <CardActions>
+            <form onSubmit={this.handleSubmit} className={classes.form}>
+              <TextField
+                label="Label"
+                style={{ margin: 8 }}
+                fullWidth
+                margin="normal"
+                variant="filled"
+                InputLabelProps={{
+                  shrink: true
+                }}
+                value={messageText}
+                onChange={this.handleChange}
+              />
+              <Button variant="contained" color="primary" type="submit">
+                Отправить
+              </Button>
+            </form>
+          </CardActions>
+        )}
       </>
     );
   }
 }
 const mapStateToProps = state => ({
   messageText: chatMessageText(state),
-  userId: userProfileSelector(state).id
+  userId: userProfileSelector(state).id,
+  permission: userPermissionsSelector(state)
 });
 export default compose(
   withStyles(styles),
